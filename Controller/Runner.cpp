2e5b649c116939af19444c31136dccf531665935
Yael_Amaya <yiap2610@gmail.com>
2018-01-23 12:38:34 -0700

    This code effectively does nothing, it doesn't even crash.

---
+++ Controller/Runner.cpp
@@ -0,0 +1,13 @@
+//
+//  Runner.cpp
+//  MyFirstCPP
+//
+//  Created by Amaya Penunuri, Yael on 1/23/18.
+//  Copyright © 2018 Amaya Penunuri, Yael. All rights reserved.
+//
+
+int main()
+{
+    
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  MyFirstCPP
+//
+//  Created by Amaya Penunuri, Yael on 1/23/18.
+//  Copyright © 2018 Amaya Penunuri, Yael. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+
+int oldmain()
+{
+    // insert code here...
+    cout << "Hello, World!\n" << endl;
+    string input;
+    getline(cin, input);
+    cout << "You said: " << input << endl;
+    return 99;
+}
